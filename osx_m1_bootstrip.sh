c07b9f1d7f3e71adc3ff64dfb4f6c8d4739210ae
Prasad Nandam <pnandam29@gmail.com>
2021-03-29 13:39:25 +0530

    Update osx_m1_bootstrip.sh
    updated the xcode install command

--- osx_m1_bootstrip.sh
+++ osx_m1_bootstrip.sh
@@ -17,7 +17,9 @@
 osascript -e 'tell application "System Preferences" to quit'
 echo "Starting bootstrapping"
 
-sudo -v
+#sudo -v
+echo "installing xcode"
+xcode-select install 
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
