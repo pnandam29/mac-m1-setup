9d489e1e1beadad47516ab707d858de3230181f3
Prasad Nandam <pnandam29@gmail.com>
2021-04-09 10:57:28 +0530

    Update osx_m1_bootstrip.sh

--- osx_m1_bootstrip.sh
+++ osx_m1_bootstrip.sh
@@ -188,21 +188,21 @@ sudo gem install ${RUBY_GEMS[@]}
 echo "Installing global npm packages..."
 npm install marked -g
 
-#echo "Configuring OSX..."
+echo "Configuring OSX..."
 #
 ## Set fast key repeat rate
 ##defaults write NSGlobalDomain KeyRepeat -int 0
 #
 ## Require password as soon as screensaver or sleep mode starts
-#defaults write com.apple.screensaver askForPassword -int 1
-#defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 #
 ## Show filename extensions by default
-#defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 #
 ## Enable tap-to-click
-#defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-#defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 #
 ## Disable "natural" scroll
 ##defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
