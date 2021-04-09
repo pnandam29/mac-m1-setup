2c65a4f195f37c14fed20d56d7da9b2f49e88531
Prasad Nandam <pnandam29@gmail.com>
2021-04-09 10:41:21 +0530

    Update osx_m1_bootstrip.sh

--- osx_m1_bootstrip.sh
+++ osx_m1_bootstrip.sh
@@ -208,6 +208,85 @@ npm install marked -g
 ## Disable "natural" scroll
 ##defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 0
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Documents/Screeshots"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+#defaults write com.apple.screencapture disable-shadow -bool true
+
+###############################################################################
+# Mac App Store                                                               #
+###############################################################################
+
+# Enable the WebKit Developer Tools in the Mac App Store
+defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+# Enable Debug Menu in the Mac App Store
+defaults write com.apple.appstore ShowDebugMenu -bool true
+
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+# Automatically download apps purchased on other Macs
+defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
+# Allow the App Store to reboot machine on macOS updates
+defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+
+###############################################################################
+# Login window
+###############################################################################
+
+# Disable Guest Account
+defaults write /Library/Preferences/com.apple.loginwindow  GuestEnabled 0
+
+# User Auto login -- Off
+defaults delete /Library/Preferences/com.apple.loginwindow autoLoginUser
+
+# Enable Show Input menu in login window
+defaults write /Library/Preferences/com.apple.loginwindow showInputMenu 1
+
+# Enable show password hint
+defaults write /Library/Preferences/com.apple.loginwindow RetriesUntilHint 1
+
+# Enable Show sleep, restart, and shutdown buttons
+defaults write /Library/Preferences/com.apple.loginwindow PowerOffDisabled 0
+
+# disable gatekeeper.
+# ref https://www.defaults-write.com/disable-gatekeeper-on-your-mac/
+
+defaults write /Library/Preferences/com.apple.security GKAutoRearm -bool false
+
+#Turning Gatekeeper back on on a Mac with the following command:
+
+#defaults delete /Library/Preferences/com.apple.security GKAutoRearm
+
+
+
+
 echo "Creating folder structure..."
 [[ ! -d Wiki ]] && mkdir Wiki
 [[ ! -d Workspace ]] && mkdir Workspace
